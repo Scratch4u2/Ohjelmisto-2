16356fe5aa919cbc7562e5531168ea1744040cb7
eemilnu <eemil.nurmi@gmail.com>
2024-03-19 10:52:33 +0200

    tuntihommia

--- Tunti&kokeilut/Luokka&oliot.py
+++ Tunti&kokeilut/Luokka&oliot.py
@@ -3,11 +3,22 @@ class Player:
         print("Luodaan uusi pelaaja")
         self.nimi = nimi
         self.score = 0
+        self.level = 1
         self.location = s_location
+    def move_to_location(self, target):
+        self.location = target
+    def up_level(self):
+        self.level = self.level + 1
+    def add_score(self):
+        self.score += self.level * 10
 
 p1 = Player("Joni", "Suomi")
 p2 = Player("Onni", "Ruotsi")
 p3 = Player("Rene","Ranska")
+
+p1.move_to_location("Norja")
+p1.up_level()
+p1.add_score()
 
 print(f"Nimi:{p1.nimi}, Score:{p1.score}, Location:{p1.location} ")
 
