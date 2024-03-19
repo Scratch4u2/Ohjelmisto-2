8b513b15dc3307242e0e66202cb3dc0d4fda58be
eemilnu <eemil.nurmi@gmail.com>
2024-03-19 10:20:41 +0200

    tuntihommia

--- Tunti&kokeilut/Luokka&oliot.py
+++ Tunti&kokeilut/Luokka&oliot.py
@@ -1,2 +1,13 @@
-class player:
-    pass
+class Player:
+    def __init__(self, nimi, s_location):
+        print("Luodaan uusi pelaaja")
+        self.nimi = nimi
+        self.score = 0
+        self.location = s_location
+
+p1 = Player("Joni", "Suomi")
+p2 = Player("Onni", "Ruotsi")
+p3 = Player("Rene","Ranska")
+
+print(f"Nimi:{p1.nimi}, Score:{p1.score}, Location:{p1.location} ")
+
