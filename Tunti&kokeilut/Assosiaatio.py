2a48ddab82422425cc27202e9ce9a2f6cee00851
eemilnu <eemil.nurmi@gmail.com>
2024-03-22 10:11:35 +0200

    tuntihommia

--- Tunti&kokeilut/Assosiaatio.py
+++ Tunti&kokeilut/Assosiaatio.py
@@ -8,5 +8,12 @@ class Kauppalista:
     def __init__(self,lista):
         self.lista = lista
 
-lista1 = Kauppalista(["Gurg","Babrik",3,"Juusto",5])
-print(lista1.lista)
+#lista1 = Kauppalista(["Gurg","Babrik",3,"Juusto",5])
+#print(lista1.lista)
+
+class Koulu:
+    def __init__(self,nimi):
+        self.nimi = nimi
+
+koulu = Koulu("Metropolia")
+print(koulu.nimi)
