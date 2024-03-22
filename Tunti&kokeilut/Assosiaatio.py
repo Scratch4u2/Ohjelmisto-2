5672ac459895903c2a8125a8aeb01e0176f6ed03
eemilnu <eemil.nurmi@gmail.com>
2024-03-22 11:10:05 +0200

    tuntihommia

--- Tunti&kokeilut/Assosiaatio.py
+++ Tunti&kokeilut/Assosiaatio.py
@@ -22,8 +22,31 @@ class Oppilas:
     def print_info(self):
         print(f"Nimi: {self.nimi}\nKoulu: {self.koulu.nimi}")
 
-koulu1 = Koulu("Metropolia")
-koulu2 = Koulu("Laurea")
+#koulu1 = Koulu("Metropolia")
+#koulu2 = Koulu("Laurea")
 
-oppilas1 = Oppilas("Rene", koulu1)
-oppilas1.print_info()
+#oppilas1 = Oppilas("Rene", koulu1)
+#oppilas1.print_info()
+
+class Kirja:
+    def __init__(self,title):
+        self.title = title
+
+class Kirjasto:
+    def __init__(self,nimi):
+        self.nimi = nimi
+        self.kirjalista = []
+    def add_kirjalista(self,kirja):
+        self.kirjalista.append(kirja)
+
+kirja1 = Kirja("Onnin kivikirja")
+kirja2 = Kirja("Renen röökikirja")
+
+kirjasto1 = Kirjasto("Oodi kirjasto")
+kirjasto1.add_kirjalista(kirja1.title)
+kirjasto1.add_kirjalista(kirja2.title)
+
+
+print(kirjasto1.nimi)
+print(kirjasto1.kirjalista)
+
