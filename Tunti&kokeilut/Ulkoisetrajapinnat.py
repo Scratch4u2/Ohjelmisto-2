29ecc4cbddb9ff6318efa31d592f61a5aa33c69c
eemilnu <eemil.nurmi@gmail.com>
2024-03-26 10:18:01 +0200

    tuntihommia

--- Tunti&kokeilut/Ulkoisetrajapinnat.py
+++ Tunti&kokeilut/Ulkoisetrajapinnat.py
@@ -4,8 +4,18 @@ response = requests.get("https://api.tvmaze.com/search/shows?q=girls")
 json = response.json()
 print(json[0]["show"]["name"])
 
-for show in json:
-    print(show["show"]["name"])
+def search_shows(search_query):
+    response = requests.get("https://api.tvmaze.com/search/shows?q=" + search_query)
+    shows = response.json()
+    print(f"Tulokset hakusanalla: {search_query}")
+    for show in shows:
+        genres = ""
+        for genre in show["show"]["genres"]:
+            genres += genre + genre
+        print(f"Sarja: {show["show"]['name']} - {genres} url: {show['show']['url']}")
+
+search_input = input("Hakusana:\n")
+search_shows(search_input)
 
 
 
