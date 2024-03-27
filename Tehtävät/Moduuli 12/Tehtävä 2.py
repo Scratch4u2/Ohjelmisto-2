78ad465875aef6cf6b655963ce5ed3abf6534211
Eemil <eemil.nurmi@gmail.com>
2024-03-27 13:10:46 +0200

    tuntihommia

--- Tehtävät/Moduuli 12/Tehtävä 2.py
+++ Tehtävät/Moduuli 12/Tehtävä 2.py
@@ -1,14 +1,29 @@
 import requests
 from geopy.geocoders import Nominatim
 
+
 def haku(hakusana):
     geolocator = Nominatim(user_agent="hakusana")
     location = geolocator.geocode(hakusana)
-    latitude = location.latitude
-    longitude = location.longitude
-    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid=cf7e316ea8b13f6f70d3db522bcec441")
-    json = response.json()
-    print("Tulokset hakusanalla:", hakusana)
-    print(json)
+    if location:
+        latitude = location.latitude
+        longitude = location.longitude
+        response = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={APIKEY TÄNNE}")
+        if response.status_code == 200:
+            json_data = response.json()
+            print("Tulokset hakusanalla:", hakusana)
+            temperature_kelvin = json_data['main']['temp']
+            lampo_celsius = temperature_kelvin - 273.15
+            saatila = json_data['weather'][0]['description']
+
+            print(f"Sää: {saatila.capitalize()}")
+            print(f"Lämpötila celsiuksina: {lampo_celsius:.2f} °C")
+        else:
+            print("Error: Ei säätietoja hakusanalla")
+    else:
+        print("Error: Paikkaa hakusanalla ei löytynyt")
+
+
 hakusana = input("Hakusana: (paikkakunta/kaupunki)\n")
 haku(hakusana)
