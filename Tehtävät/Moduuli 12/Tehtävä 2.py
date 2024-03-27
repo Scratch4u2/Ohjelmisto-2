3bef044e9506212302edf7321ff83c2bdc272b38
Eemil <eemil.nurmi@gmail.com>
2024-03-27 13:16:27 +0200

    tuntihommia

--- Tehtävät/Moduuli 12/Tehtävä 2.py
+++ Tehtävät/Moduuli 12/Tehtävä 2.py
@@ -5,11 +5,12 @@ from geopy.geocoders import Nominatim
 def haku(hakusana):
     geolocator = Nominatim(user_agent="hakusana")
     location = geolocator.geocode(hakusana)
+    apikey = APIKEY TÄHÄN
     if location:
         latitude = location.latitude
         longitude = location.longitude
         response = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={APIKEY TÄNNE}")
+            f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={apikey}")
         if response.status_code == 200:
             json_data = response.json()
             print(f"Tulokset hakusanalla:{hakusana}")
