7474bf6ad3534196650bfc0811925834b9ac0ec5
Eemil <eemil.nurmi@gmail.com>
2024-03-27 11:17:58 +0200

    tuntihommia

---
+++ Tehtävät/Moduuli 11/Tehtävä 1.py
@@ -0,0 +1,24 @@
+class Julkaisu:
+    def __init__(self, tyyppi, nimi):
+        self.tyyppi = tyyppi
+        self.nimi = nimi
+    def tulosta_tiedot(self):
+        print(f"Tyyppi: {self.tyyppi}\nNimi: {self.nimi}")
+
+class Kirja(Julkaisu):
+    def __init__(self, tyyppi, nimi, kirjoittaja, sivumaara):
+        super().__init__(tyyppi, nimi)
+        self.kirjoittaja = kirjoittaja
+        self.sivumaara = sivumaara
+
+
+class Lehti(Julkaisu):
+    def __init__(self, tyyppi, nimi, paatoimittaja):
+        super().__init__(tyyppi, nimi)
+        self.paatoimittaja = paatoimittaja
+
+AkuAnkka = Lehti("Lehti", "Aku_Ankka","Aki Hyyppä")
+Hytti_n6 = Kirja("Kirja", "Hytti_n6", "Rosa Liksom", 200)
+
+AkuAnkka.tulosta_tiedot()
+Hytti_n6.tulosta_tiedot()
