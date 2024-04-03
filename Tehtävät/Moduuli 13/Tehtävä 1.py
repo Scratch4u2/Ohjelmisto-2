1ca8a5ec10df56d1303fadce375020d3408289bd
Eemil <eemil.nurmi@gmail.com>
2024-04-03 13:50:28 +0300

    kotitehtäviä

--- Tehtävät/Moduuli 13/Tehtävä 1.py
+++ Tehtävät/Moduuli 13/Tehtävä 1.py
@@ -1,9 +1,36 @@
-def on_alkuluku(numero):
-    if numero < 2:
-        return False
-    for n in range(2, int(numero/2)+1):
-        if numero % n == 0:
-            return False
-    else:
-        return True
+from flask import Flask, Response
+import json
 
+app = Flask(__name__)
+
+#http://127.0.0.1:3000/alkuluku/<luku>
+@app.route('/alkuluku/<luku>')
+def on_alkuluku(luku):
+    try:
+        luku = int(luku)
+        onko = None
+        if luku < 2:
+            onko = False
+        else:
+            onko = True
+            for n in range(2, int(luku / 2) + 1):
+                if luku % n == 0:
+                    onko = False
+                    break
+        tilakoodi = 200
+        vastaus = {
+            "Number": luku,
+            "isPrime": onko
+        }
+    except ValueError:
+        tilakoodi = 400
+        vastaus = {
+            "status": tilakoodi,
+            "teksti": "Virheellinen yhteenlaskettava"
+        }
+    json_vast = json.dumps(vastaus)
+    return Response(response=json_vast, status=tilakoodi, mimetype="application/json")
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
