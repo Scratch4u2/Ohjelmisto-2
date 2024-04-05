5ee54b9e9ba9687cb94eba5d01f0c5f700bc60d0
Eemil <eemil.nurmi@gmail.com>
2024-04-05 13:27:00 +0300

    Tuntihommia

--- Javascript/Tehtävät/Moduuli 1/Tehtävä 10/Tht10.js
+++ Javascript/Tehtävät/Moduuli 1/Tehtävä 10/Tht10.js
@@ -5,9 +5,9 @@ function rollDice() {
 function probability(numdice, sumeyes){
     let correct = 0
 
-    for (let i; i<10000; i++){
+    for (let i = 0; i<10000; i++){
         let throwsum = 0
-        for (let j; j < numdice; j++){
+        for (let j = 0; j < numdice; j++){
             throwsum += rollDice()
         }
         if (throwsum === sumeyes){
@@ -21,6 +21,6 @@ function probability(numdice, sumeyes){
 }
 const numdice = parseInt(prompt("Number of dice"))
 const sumeyes = parseInt(prompt("Wanted eyesum"))
-const answer = probability(numdice,sumeyes)
+const answer = probability(numdice,sumeyes).toFixed(2) * 100
 const print = document.querySelector('p')
-print.textContent = 'Probability of ' + sumeyes + ' with ' + numdice + ' dice is: ' + answer;
+print.textContent = 'Probability of ' + sumeyes + ' with ' + numdice + ' dice is: ' + answer + '%';
