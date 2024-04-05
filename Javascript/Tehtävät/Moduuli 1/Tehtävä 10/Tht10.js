3da0210d52fb7679bbdb9c707e9b05d6b730f153
Eemil <eemil.nurmi@gmail.com>
2024-04-05 13:44:19 +0300

    Tuntihommia

--- Javascript/Tehtävät/Moduuli 1/Tehtävä 10/Tht10.js
+++ Javascript/Tehtävät/Moduuli 1/Tehtävä 10/Tht10.js
@@ -4,7 +4,7 @@ function probability(numDice, sumEyes) {
     for (let i = 0; i < 10000; i++) {
         let throwSum = 0;
         for (let j = 0; j < numDice; j++) {
-            throwSum += Math.floor(Math.random() * 6) + 1;
+            throwSum += Math.floor(Math.random() * 5) + 1;
         }
         if (throwSum === sumEyes) {
             correct += 1;
