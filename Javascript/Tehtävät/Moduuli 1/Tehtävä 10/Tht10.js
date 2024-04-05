80624c116ecfa02633eaedf233bc17d6e4f16a4b
Eemil <eemil.nurmi@gmail.com>
2024-04-05 13:30:31 +0300

    Tuntihommia

--- Javascript/Tehtävät/Moduuli 1/Tehtävä 10/Tht10.js
+++ Javascript/Tehtävät/Moduuli 1/Tehtävä 10/Tht10.js
@@ -1,26 +1,28 @@
 function rollDice() {
-    return Math.random() * 6;
+    return Math.floor(Math.random() * 6) + 1;
 }
 
-function probability(numdice, sumeyes){
-    let correct = 0
+function probability(numDice, sumEyes) {
+    let correct = 0;
 
-    for (let i = 0; i<10000; i++){
-        let throwsum = 0
-        for (let j = 0; j < numdice; j++){
-            throwsum += rollDice()
+    for (let i = 0; i < 10000; i++) {
+        let throwSum = 0;
+        for (let j = 0; j < numDice; j++) {
+            throwSum += rollDice();
         }
-        if (throwsum === sumeyes){
-            correct += 1
-        }
-        else {
-
+        if (throwSum === sumEyes) {
+            correct += 1;
         }
     }
-    return correct / 10000
+
+    return correct / 10000;
 }
-const numdice = parseInt(prompt("Number of dice"))
-const sumeyes = parseInt(prompt("Wanted eyesum"))
-const answer = probability(numdice,sumeyes).toFixed(2) * 100
-const print = document.querySelector('p')
-print.textContent = 'Probability of ' + sumeyes + ' with ' + numdice + ' dice is: ' + answer + '%';
+
+const numDice = parseInt(prompt("Number of dice:"));
+const sumEyes = parseInt(prompt("Sum of the eyes:"));
+
+const answer = probability(numDice, sumEyes).toFixed(4);
+console.log(`The probability of getting a sum of ${sumEyes} with ${numDice} dice is approximately ${answer}.`);
+
+const print = document.querySelector('p');
+print.textContent = `Probability of ${sumEyes} with ${numDice} dice is: ${(answer * 100).toFixed(1)}%.`;
