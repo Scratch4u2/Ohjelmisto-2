2a4cc309cf3c39b511eb94844c02e2931a7e93cc
Eemil <eemil.nurmi@gmail.com>
2024-04-05 13:58:25 +0300

    Tuntihommia

--- Javascript/Tehtävät/Moduuli 1/Tehtävä 10/Tht10.js
+++ Javascript/Tehtävät/Moduuli 1/Tehtävä 10/Tht10.js
@@ -1,22 +1,22 @@
 function probability(numDice, sumEyes) {
-    let correct = 0;
+  let correct = 0;
 
-    for (let i = 0; i < 10000; i++) {
-        let throwSum = 0;
-        for (let j = 0; j < numDice; j++) {
-            throwSum += Math.floor(Math.random() * 5) + 1;
-        }
-        if (throwSum === sumEyes) {
-            correct += 1;
-        }
+  for (let i = 0; i < 10000; i++) {
+    let throwSum = 0;
+    for (let j = 0; j < numDice; j++) {
+      throwSum += Math.floor(Math.random() * 5) + 1;
     }
-
-    return correct / 10000;
+    if (throwSum === sumEyes) {
+      correct += 1;
+    }
+  }
+  return correct / 10000;
 }
 
-const numDice = parseInt(prompt("Number of dice:"));
-const sumEyes = parseInt(prompt("Sum of the eyes:"));
+const numDice = parseInt(prompt('Number of dice:'));
+const sumEyes = parseInt(prompt('Sum of the eyes:'));
 const answer = probability(numDice, sumEyes).toFixed(3);
 
 const print = document.querySelector('p');
-print.textContent = 'Probability of ' + sumEyes + ' with ' + numDice + ' dice is:' + answer * 100 + '%';
+print.textContent = 'Probability of ' + sumEyes + ' with ' + numDice +
+    ' dice is:' + answer * 100 + '%';
