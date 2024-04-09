cda136b56e693897313b1791eb4f84c3ee6de827
Eemil <eemil.nurmi@gmail.com>
2024-04-09 09:34:07 +0300

    Tuntihommia

--- Javascript/Tunti_kokeilut/Js_tunti3/Taulukkojuttu.js
+++ Javascript/Tunti_kokeilut/Js_tunti3/Taulukkojuttu.js
@@ -1,27 +1,22 @@
-
-
 const names = [];
 
-names[0] = "Aku"
-names[1] = "Iines"
-console.log(names)
+names[0] = 'Aku';
+names[1] = 'Iines';
+console.log(names);
 
-names.push("Roope")
-console.log(names)
+names.push('Roope');
+console.log(names);
 
-names.pop()
-console.log(names)
+names.pop();
+console.log(names);
 
-const lastName = names.pop()
-console.log(names)
-console.log(lastName)
+const lastName = names.pop();
+console.log(names);
+console.log(lastName);
 
-const ages = [4,5,6];
+const ages = [4, 5, 6];
 //console.log(ages.length)
 
-for (let i=0; i < ages.length; i++){
-  console.log(ages[i])
+for (let i = 0; i < ages.length; i++) {
+  console.log(ages[i]);
 }
-
-
-
