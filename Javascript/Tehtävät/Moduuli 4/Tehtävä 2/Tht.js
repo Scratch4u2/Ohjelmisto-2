fd792c9b1c458c2c3156716f2b6ab023aee4b8f1
Eemil <eemil.nurmi@gmail.com>
2024-04-12 15:26:20 +0300

    Tuntihommia

--- Javascript/Tehtävät/Moduuli 4/Tehtävä 2/Tht.js
+++ Javascript/Tehtävät/Moduuli 4/Tehtävä 2/Tht.js
@@ -1,2 +1,13 @@
-const search = document.querySelector('search')
-searc.addEventListener('submit', async function(event))
+const search = document.querySelector('#search');
+search.addEventListener('submit', async function(event) {
+  event.preventDefault();
+
+  const value_from_input = document.querySelector('#query').value;
+  try {
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${value_from_input}`);
+    const jsonData = await response.json();
+    console.log(jsonData);
+  } catch (error) {
+    console.log('Error:', error);
+  }
+});
