7570a95d0aca5f206d0b55138aeece7513e78b8b
Eemil <eemil.nurmi@gmail.com>
2024-04-12 10:03:56 +0300

    Tuntihommia

--- Javascript/Tunti_kokeilut/Js_tunti4/dom.js
+++ Javascript/Tunti_kokeilut/Js_tunti4/dom.js
@@ -1,5 +1,13 @@
 function locationsuccess (){
   console.log("Käyttäjä paikannettu")
 }
+function locationError(){
+    console.log("Käyttäjää ei löydetty")
+
+}
+
+const locationOptions = {
+  timeout: 1000
+}
 
 navigator.geolocation.getCurrentPosition(locationsuccess);
