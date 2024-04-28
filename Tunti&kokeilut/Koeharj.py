b86ac8be192b661b75583949a0120915963c6061
Eemil <eemil.nurmi@gmail.com>
2024-04-28 18:22:49 +0300

    tuntijuttuja

--- Tunti&kokeilut/Koeharj.py
+++ Tunti&kokeilut/Koeharj.py
@@ -10,6 +10,8 @@ class Engine():
 
 
 class Car(Engine):
+    car_count = 0
+
     def __init__(self, price, make, year, engine):
         # Call the __init__ method of the parent class (Engine) to initialize the engine's model and type
         super().__init__(engine.model, engine.type)
@@ -19,24 +21,34 @@ class Car(Engine):
         self.make = make
         self.year = year
         self.engine = engine  # Store the Engine object associated with the Car
+        Car.car_count += 1
 
     def print_info(self):
         # Call the print_info method of the parent class (Engine) to print engine details
         super().print_info()
 
         # Print Car-specific attributes: price, make, and year
-        print(self.price, self.make, self.year)
+        print("Car details:", self.price, self.make, self.year, "Engine details:", self.engine.model, self.engine.type)
+
+        # Print engine details
+        # print("Engine details:", self.engine.model, self.engine.type)
 
 
 # Create an Engine object
 bensiini1 = Engine("123")
 # Print information about the Engine
-bensiini1.print_info()
+#bensiini1.print_info()
 
 # Create another Engine object
 sahko = Engine("456", "Electric")
+#sahko.print_info()
 
 # Create a Car object and associate it with the first Engine object
 auto1 = Car("30000", "Toyota", 2019, bensiini1)
 # Print information about the Car, including Engine details
 auto1.print_info()
+
+auto2 = Car("50000", "Lexus", 2024, sahko)
+auto2.print_info()
+
+print(Car.car_count)
